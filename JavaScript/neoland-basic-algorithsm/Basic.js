9938b5d9b72a1887b8e7ebbde8a0fd48fdb5187f
mamupe <mario.muperez@gmail.com>
2022-09-29 17:00:44 +0200

    feat: actualización

--- JavaScript/neoland-basic-algorithsm/Basic.js
+++ JavaScript/neoland-basic-algorithsm/Basic.js
@@ -1,23 +1,33 @@
 //"Iteration#1"
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 let myFavoriteHero = "Hulk";
+//1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 let x = 50;
+//1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
 let h = 5;
 let y = 10;
+//Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
 
 //"Iteration#2"
-/*  const character = { name: "Jack Sparrow", age: 10 };
-age = 25;  */
-
+//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+console.log(character);
+//1.2 Declara 3 variables con los nombres y valores siguientes 
 let firstName = "Jon";
 let lastName = "Snow";
 let age = 24;
 console.log("Soy " + firstName + " " + lastName + ", " + "tengo " + age + " años y me gustan los lobos");
+//1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes.
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 console.log(toy1.price + toy2.price);
 
+/* 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+basePrice más el valor de la variable globalBasePrice. */
 
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
@@ -35,24 +45,35 @@ console.log(car2.finalPrice);
 
 
 //"Iteration#3"
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 let num1 = 10;
 let num2 = 5;
 
-console.log(num1 + num2);
+console.log(num1 * num2);
 
+//1.2 Divide 10 por 2 y muestra el resultado en un console.
 let num3 = 10;
 let num4 = 2;
-console.log(num3 * num4);
+console.log(num3 / num4);
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
 
 const div1 = 15;
 const div2 = 9;
 const resto = (div1 % div2);
 console.log(resto);
 
+//1.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+
+
 let p = 10;
 let j = 5;
 o = (p + j);
 console.log(o);
+
+//1.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+
+
 
 let c = 10;
 let m = 5;
@@ -61,18 +82,18 @@ console.log(i);
 
 //Iteración #4
 
-
+//1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 let Hulk = avengers[0];
 console.log(Hulk);
-
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
 const avengers1 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 avengers1.splice(0, 1, 'IRONMAN');
 console.log(avengers1);
-
+//1.3 console numero de elementos en el array usando la propiedad correcta de Array.
 const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers2.length);
-
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". 
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty", "Summer");
 const last = rickAndMortyCharacters[rickAndMortyCharacters.length - 1];
@@ -84,14 +105,14 @@ console.log(rickAndMortyCharacters1);
 const firstLast = rickAndMortyCharacters1[0];
 console.log(rickAndMortyCharacters1[(rickAndMortyCharacters1.length) -1]);
 
-//Elimina el segundo elemento del array y muestra el array por consola.
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
 const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 rickAndMortyCharacters2.splice(1, 1)
 console.log(rickAndMortyCharacters2); 
 
 
 //Iteración #5
-
+// 
 const number1 = 10;
 const number2 = 20;
 const number3 = 2;
@@ -134,25 +155,29 @@ for (let x = 0; x < 10; x++) {
 
 //1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
 
-let moderatIII = new Array();
-            moderatIII[0] = "Eating hooks";
-            moderatIII[1] = "Running";
-            moderatIII[2] = "Finder";
-            moderatIII[3] = "Ghostmother";
-            moderatIII[4] = "Reminder";
-            moderatIII[5] = "Intruder";
-            moderatIII[6] = "Animal trails";    
-
-//console.log(moderatIII.splice(2,2));
-
-
-/* console.log(moderatIII); */
+for (let x = 0; x < 10; x++) {
+  if (x % 2 == 0) {
+    console.log(x);
+    
+  }
+}
 
 
 
-//console.log(moderatIII.splice(2,2,'Ethereal'));
+
+/* 1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+y cambia el mensaje en la décima vuelta a 'Dormido!'. */
+
+let ovejas = [1,2,3,4,5,6,7,8,9,10];
+
+for (let index = 0; index < ovejas.length; index++) {
+  if(ovejas[index] < 10) {
+		console.log('Intentando dormir')
+	} else {
+    console.log("Vete a dormir")
+  }
+}
 
 
-
-let nuevo = (moderatIII.splice(2,2,'Ethereal','Ethereal Remix vol.1','Ethereal Remix vol.2')); 
-console.log(nuevo);
