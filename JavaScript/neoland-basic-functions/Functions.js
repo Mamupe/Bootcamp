5902c0ad2b14947825beecb652b158e401e823f6
mamupe <mario,muperez@gmail.com>
2022-10-02 20:39:43 +0200

    feat update

--- JavaScript/neoland-basic-functions/Functions.js
+++ JavaScript/neoland-basic-functions/Functions.js
@@ -19,7 +19,7 @@ Completa la función que tomando un array de strings como argumento devuelva el 
 
 Puedes usar este array para probar tu función: */
 
- const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+/*  const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 const findLongestWord = (avengers) => {
   let longestWord = "";
   for (const avenger of avengers) {
@@ -31,15 +31,27 @@ const findLongestWord = (avengers) => {
 return longestWord;
 }
  const findLongestWord = avengers;
-console.log(avengers);
+console.log(avengers); */
 /* **Iteración #3: Calcular la suma**
 
 Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
 Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. 
 
 Puedes usar este array para probar tu función: */
-/* const numbers = [1, 2, 3, 5, 45, 37, 58];
+const numbers = [1, 2, 3, 5, 45, 37, 58];
 
-function sumAll(numbers) {
+
+  let total = 0
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+}
+console.log(total);
+
+
+//**Iteración #4: Calcular el promedio**
+
+//Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
   
-} */
+}
