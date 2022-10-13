bf4876b72bdd7eec461da84018b0e0bcc4e0b11c
mamupe <mario,muperez@gmail.com>
2022-10-13 15:48:14 +0200

    feat: empezando eventos

--- JavaScript/neoland-dom/iteracion2/dom.js
+++ JavaScript/neoland-dom/iteracion2/dom.js
@@ -2,15 +2,23 @@
 //2.1 Inserta dinamicamente en un html un div vacio con javascript.
 const div1 = document.createElement("div");
 const empty = document.body.appendChild(div1);
-console.log(empty);
+
 //2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
 const div2 = document.createElement("div");
-const parrafo = document.createElement("p");
-const empty2 = document.body.appendChild(div2);
-const empty3 = div2.appendChild(parrafo);
-console.log(empty3);
+div2.innerHTML += `
+<p>..</p>
+`;
 
 //2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+const divloop = document.createElement("div");
+document.body.appendChild(divloop);
+for (let index = 0; index < 6; index++) {
+    divloop.innerHTML += `
+    <p></p>
+    `
+};
+
+    
 
 
 //2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
@@ -18,17 +26,37 @@ const parrafo2 = document.createElement("p");
 const empty4 = document.body.appendChild(parrafo2);
 const textop = document.createTextNode('Soy dinámico');
 parrafo2.appendChild(textop);
-console.log(parrafo2);
+
 //2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
 const htmlh2 = document.querySelector('.fn-insert-here');
 let lubba = htmlh2.innerHTML = 'Wubba Lubba dub dub';
-console.log(lubba);
+
 //2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
-//const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const insetrUl = document.createElement("ul");
+document.body.appendChild(insetrUl);
+for (const item of apps) {
+    insetrUl.innerHTML += `
+    
+    <li>${item}</li>
+    `
+    
+};
+  
+   
 
 //2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+var remove = document.getElementById('.fn-remove-me');
+remove.classList.remove('p');
+
 
 //2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
 //Recuerda que no solo puedes insertar elementos con .appendChild.
+const medio = document.querySelector("div");
+medio.insertAdjacentHTML("afterend", '<p>Voy en medio</p>')
 
 //2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+const dentro = document.querySelector(".fn-insert-here");
+dentro.innerHTML += `
+<p>Voy dentro</p>
+`
