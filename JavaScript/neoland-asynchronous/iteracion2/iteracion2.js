3713f5c8dd9ed81fc68d83b7e8cfbd2934066bd4
mamupe <mario,muperez@gmail.com>
2022-10-17 15:46:19 +0200

    feat: done

--- JavaScript/neoland-asynchronous/iteracion2/iteracion2.js
+++ JavaScript/neoland-asynchronous/iteracion2/iteracion2.js
@@ -1,65 +1,47 @@
-/* 2.1 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando 
-fetch() para hacer una consulta a la api cuando se haga click en el botón, 
-pasando como parametro de la api, el valor del input.*/
-
-const baseUrl = "https://api.nationalize.io";
-
-const getData = async (name) => {
-  const data = await fetch(`https://api.nationalize.io?name=${name}`);
-  const dataJson = await data.json();
-  //console.log(dataJson);
-  return dataJson;
-};
-/* const clickEvent = () => {
-  const button = document.querySelector("button");
-  button.addEventListener("click", () => {
-    const input = document.querySelector("input").value;
-    getData(input);
-  });
-}; */
-//getData();
-//clickEvent();
-
-/*  2.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
-a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
-EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
-de MZ. */
-
-const ul = document.createElement("ul");
-document.body.appendChild(ul);
-
-let printData = (personName) => {
-  personName.country.forEach((element) => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-<p>El nombre ${personName.name} tiene una ${element.probability} % de ser de ${element.country_id}</p>
-`;
-ul.appendChild(li)
-const deleteButton = document.createElement("button");
-li.appendChild(deleteButton);
-deleteButton.innerText = `X`;
-deleteButton.addEventListener("click", () => {
-    li.remove()});
-  });
-  
-};
-
-
-
-const clickEvent = () => {
-    const button = document.querySelector("button");
-    button.addEventListener("click", async () => {
-      const input = document.querySelector("input").value;
-      const dataPrint = await getData(input);
-      printData(dataPrint);
+/* 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando 
+async-await. */
+/* const runTimeOut = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
     })
+
+    promise.then(() => {console.log('Time out!')})
 };
 
-clickEvent();
+runTimeOut(); */
 
-/* 2.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
-de los p que hayas insertado y que si el usuario hace click en este botón 
-eliminemos el parrafo asociado.  */
+const runTimeOut1 = async () => {
+    await new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    console.log('Time out!')
+};
+
+runTimeOut1()
 
 
 
+/* 2.2 Convierte la siguiente función con un fetch utilizando async-await. 
+Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador; */
+/* function getCharacters () {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters(); */
+
+const getCharacters = async () => {
+    try{
+    const characters = await fetch('https://rickandmortyapi.com/api/character');
+    const charactersJson = await characters.json();
+    console.log(charactersJson)
+    } catch (error) {
+        console.log("Error", error)
+    }
+    return charactersJson
+}
+getCharacters();
